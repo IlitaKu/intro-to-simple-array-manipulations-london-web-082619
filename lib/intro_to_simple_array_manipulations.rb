638c92e41130e81ccdc17a2a491452ccf096aa9b
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-21 20:11:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,3 +22,24 @@ end
 def using_insert(array, elem)
   array.insert(4,elem)
 end
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
+end
+def using_delete(array, str)
+  array.delete(str)
+end
+def using_delete_at(array,int)
+  array.delete_at(int)
+end
+
+
+
+
+
+
+
+
+
